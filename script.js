3e1f2675066f497064d996532ba7ed3942dea0c7
revathypr095-rgb <revathypr095@gmail.com>
2025-11-25 15:31:33 +0530

    Add calculator files

---
+++ script.js
@@ -0,0 +1,33 @@
+const resultInput = document.getElementById('result');
+
+function appendNumber(number) {
+    resultInput.value += number;
+}
+
+function appendOperator(operator) {
+    if (resultInput.value !== '' && !isOperator(resultInput.value.slice(-1))) {
+        resultInput.value += operator;
+    }
+}
+
+function calculateResult() {
+    if (resultInput.value !== '' && !isOperator(resultInput.value.slice(-1))) {
+        try {
+            resultInput.value = eval(resultInput.value);
+        } catch (error) {
+            resultInput.value = 'Error';
+        }
+    }
+}
+
+function clearResult() {
+    resultInput.value = '';
+}
+
+function deleteLastChar() {
+    resultInput.value = resultInput.value.slice(0, -1);
+}
+
+function isOperator(char) {
+    return ['+', '-', '*', '/'].includes(char);
+}
